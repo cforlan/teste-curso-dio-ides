8fd36282b4a5436d0eea67d5de1c778edebd5529
CFORLAN <cforlan@gmail.com>
2021-09-01 08:24:22 -0300

    Firsts commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Livros livros = new Livros();
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));*/
+	}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+}
