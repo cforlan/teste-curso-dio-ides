347f7e46481a9fe79b517a6b9c72b46b23ca2a01
CFORLAN <cforlan@gmail.com>
2021-09-01 15:44:19 -0300

    Inicio do exercicio 01

---
+++ exercicios/src/exercicios/exercicio01.java
@@ -0,0 +1,16 @@
+package exercicios;
+//Programa para calcular média salarial
+public class exercicio01 {
+    public static void main(String[] args) {
+        double n1, n2, n3, n4, media;
+
+        n1 = 3453.21;
+        n2 = 3498.43;
+        n3 = 7902.09;
+        n4 = 12932.00;
+
+        media = (n1 + n2 + n3 + n4) / 4;
+
+        System.out.println("A média salarial da empresa é de R$ " + media);
+    }
+}
